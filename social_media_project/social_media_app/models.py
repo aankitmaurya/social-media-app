a0d54bab56b50222b0ad44f834dce4d4aa9b20d4
Ankit Kumar <ankitkumar@tessact.com>
2023-05-21 15:54:05 +0530

    created models

--- social_media_project/social_media_app/models.py
+++ social_media_project/social_media_app/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 # Profile
 class Profile(models.Model):
-    user = models.ForeignKey(User, related_name='user_obj', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='user_obj', on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True, upload_to='profile_images')
     location = models.CharField(max_length=100, blank=True, null=True)
@@ -15,3 +15,26 @@ class Profile(models.Model):
     
     class Meta:
         db_table = "profile"
+
+
+# Post
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
+    caption = models.TextField(null=True, blank=True)
+    image = models.ImageField(blank=True, null=True, upload_to='post_images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "post"
+
+
+# LikePost
+class LikePost(models.Model):
+    post = models.ForeignKey(Post, related_name='like_post', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='like_post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "like_post"
