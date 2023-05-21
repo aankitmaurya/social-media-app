f17679ba417502a6e99bb6b15ff39147fc3fbb85
kabeer7284 <kabeer7284@gmail.com>
2023-05-21 14:16:44 +0530

    true - false

--- social_media_project/social_media_app/models.py
+++ social_media_project/social_media_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 # User
 User = get_user_model()
 
+
 # Profile
 class Profile(models.Model):
     user = models.ForeignKey(User, related_name='user_obj', on_delete=models.CASCADE)
@@ -11,7 +12,7 @@ class Profile(models.Model):
     image = models.ImageField(blank=True, null=True, upload_to='profile_images')
     location = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
+    updated_at = models.DateTimeField(auto_now=False)
+
     class Meta:
         db_table = "profile"
